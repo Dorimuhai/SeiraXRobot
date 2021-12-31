16ed9a73b0c3db4ae4d98d8794b0181b3b7513b2
seth <87837653+Dorimuhai@users.noreply.github.com>
2022-01-01 02:05:10 +0700

    Update memify.py

--- SeiraRobot/modules/memify.py
+++ SeiraRobot/modules/memify.py
@@ -39,7 +39,7 @@ async def drawText(image_path, text):
     if os.name == "nt":
         fnt = "ariel.ttf"
     else:
-        fnt = "./SiestaRobot/resources/default.ttf"
+        fnt = "./SeiraRobot/resources/default.ttf"
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
     if ";" in text:
         upper_text, lower_text = text.split(";")
