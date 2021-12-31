d92eb34288dc873106fc441ce9416577b1fc1db8
seth <87837653+Dorimuhai@users.noreply.github.com>
2022-01-01 01:53:05 +0700

    Update disasters.py

--- SeiraRobot/modules/disasters.py
+++ SeiraRobot/modules/disasters.py
@@ -3,7 +3,7 @@ import json
 import os
 from typing import Optional
 
-from lunaBot import (
+from SeiraRobot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -13,7 +13,7 @@ from lunaBot import (
     WOLVES,
     dispatcher,
 )
-from lunaBot.modules.helper_funcs.chat_status import (
+from SeiraRobot.modules.helper_funcs.chat_status import (
     dev_plus,
     sudo_plus,
     whitelist_plus,
@@ -24,7 +24,7 @@ from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.utils.helpers import mention_html
 
-ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "lunaBot/elevated_users.json")
+ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "SeiraRobot/elevated_users.json")
 
 
 def check_user_id(user_id: int, context: CallbackContext) -> Optional[str]:
