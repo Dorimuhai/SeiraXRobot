bbf16cd5f2ba65fcacd15b35794e7ec5c357a6da
seth <87837653+Dorimuhai@users.noreply.github.com>
2022-01-01 00:09:00 +0700

    Update wiki.py

--- SeiraRobot/modules/wiki.py
+++ SeiraRobot/modules/wiki.py
@@ -1,6 +1,6 @@
 import wikipedia
-from SieraRobot import dispatcher
-from SieraRobot.modules.disable import DisableAbleCommandHandler
+from SeiraRobot import dispatcher
+from SeiraRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
