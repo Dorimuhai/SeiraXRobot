2a80e760baf409fbc75064a28f6a7dd78d3c9b75
seth <87837653+Dorimuhai@users.noreply.github.com>
2022-01-01 00:18:31 +0700

    Update config.py

--- SeiraRobot/config.py
+++ SeiraRobot/config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/SiestaRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/SeiraRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -17,9 +17,9 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 945137470  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "saint_foire"
-    SUPPORT_CHAT = "machinaxsupport"  # Your own group for support, do not add the @
+    OWNER_ID = 2048978097  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "xyzsethhh"
+    SUPPORT_CHAT = "seirasupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001180007354
     )  # Prints any new group the bot is added to, prints just the name and ID.
