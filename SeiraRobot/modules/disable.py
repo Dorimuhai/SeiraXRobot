00c2ce9c446525c846452a005d859b1c98a4942d
seth <87837653+Dorimuhai@users.noreply.github.com>
2021-12-31 23:52:38 +0700

    Update disable.py

--- SeiraRobot/modules/disable.py
+++ SeiraRobot/modules/disable.py
@@ -27,7 +27,7 @@ if is_module_loaded(FILENAME):
         is_user_admin,
         user_admin,
     )
-    from Seira.modules.sql import disable_sql as sql
+    from SeiraRobot.modules.sql import disable_sql as sql
 
     DISABLE_CMDS = []
     DISABLE_OTHER = []
