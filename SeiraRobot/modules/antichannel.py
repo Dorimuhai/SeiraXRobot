d3778d33893e6f9689ef1f4bed3bb1ee3fbb4897
seth <87837653+Dorimuhai@users.noreply.github.com>
2022-01-01 15:26:58 +0700

    Update antichannel.py

--- SeiraRobot/modules/antichannel.py
+++ SeiraRobot/modules/antichannel.py
@@ -4,7 +4,7 @@ from telegram.ext.filters import Filters
 from telegram import Update, message, ParseMode
 from telegram.ext import CallbackContext
 
-from SeiraRobot.modules.helper_funcs.decorators import siestacmd, siestamsg
+from SeiraRobot.modules.helper_funcs.decorators import seiracmd, seiramsg
 from SeiraRobot.modules.helper_funcs.channel_mode import user_admin, AdminPerms
 from SeiraRobot.modules.sql.antichannel_sql import antichannel_status, disable_antichannel, enable_antichannel
 
