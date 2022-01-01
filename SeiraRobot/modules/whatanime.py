d58875f4556e9005b3d353cd14f39462aef0fe40
seth <87837653+Dorimuhai@users.noreply.github.com>
2022-01-01 15:38:24 +0700

    Update whatanime.py

--- SeiraRobot/modules/whatanime.py
+++ SeiraRobot/modules/whatanime.py
@@ -16,7 +16,7 @@ from telethon.tl.types import (
 )
 from telethon.utils import is_image, is_video
 
-from SeiraRobot import register as tomori
+from SeiraRobot.events import register as tomori
 
 
 
