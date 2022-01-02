9c7da5f7e53aba4b8616586b715b06a2f075a1ea
seth <87837653+Dorimuhai@users.noreply.github.com>
2022-01-03 02:53:01 +0700

    Update admin.py

--- SeiraRobot/modules/admin.py
+++ SeiraRobot/modules/admin.py
@@ -875,7 +875,12 @@ async def bug(bot, message: Message):
             await message.reply_text(reply_text("teks")
         )
         return await message.reply_text("dah kekirim ya ke grup")
-            await bot.send_message(LOG_CHAT_ID,"""pesannya"""
+            await 
+            bot.send_message(LOG_CHAT_ID,f"""
+            Siap kak
+            Emm okay
+            Iya bang buset
+            """
         )
 
 
