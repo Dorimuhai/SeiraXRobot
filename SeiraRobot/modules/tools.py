b8b760dec89fe699ffde14404225d2d998e71e99
seth <87837653+Dorimuhai@users.noreply.github.com>
2022-01-02 21:51:26 +0700

    Update tools.py

--- SeiraRobot/modules/tools.py
+++ SeiraRobot/modules/tools.py
@@ -23,12 +23,11 @@ Link To File:
 ❂ /up: reply to a direct download link to upload it to telegram as files
  
  File To Link:
-❂ /transfersh: reply to a telegram file to upload it on transfersh and get direct download link
-❂ /tmpninja: reply to a telegram file to upload it on tmpninja and get direct download link
- File Or Text To Telegraph:
 ❂ /tgm : Get Telegraph Link Of Replied Media
 ❂ /tgt: Get Telegraph Link of Replied Text
 
  Tagger
 ❂ /tagall : for tag everyone at least 100 member first.
+❂ /all <reason> : To tag all members in the group.
+❂ /cancel : To stop tagging all members in the group.
 """
