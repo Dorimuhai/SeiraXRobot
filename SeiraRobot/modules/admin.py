4dd401de2539b0bbe8c47f94248343988f80a999
seth <87837653+Dorimuhai@users.noreply.github.com>
2022-01-04 21:59:42 +0700

    Update admin.py

--- SeiraRobot/modules/admin.py
+++ SeiraRobot/modules/admin.py
@@ -837,7 +837,7 @@ def adminlist(update, context):
                 normal_admin_list.append(name)
 
     for admin in normal_admin_list:
-        text += "\n<code> • </code>{}".format(admin)
+        text += "\n<code> ├ </code>{}".format(admin)
 
     for admin_group in custom_admin_list.copy():
         if len(custom_admin_list[admin_group]) == 1:
