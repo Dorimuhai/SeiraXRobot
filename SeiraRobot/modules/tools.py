e50421e8dae10a6650477a9b3f710556de2d125f
seth <87837653+Dorimuhai@users.noreply.github.com>
2022-01-20 22:51:55 +0700

    added multi lang

--- SeiraRobot/modules/tools.py
+++ SeiraRobot/modules/tools.py
@@ -1,34 +1,8 @@
+from SeiraRobot.modules.language import gs
+
+
+def helps(chat):
+    return gs(chat, "tools_help")
+
+
 __mod_name__ = "Tools"
-
-__help__ = """
-*Sangmata Info bot*
-❂ /sg <reply>*:* To check history name
-
-*Date-time-Weather*
-❂ /time <country code>*:* Gives information about a timezone.
-❂ /weather <city>*:* Get weather info in a particular place.
-❂ /wttr <city>*:* Advanced weather module, usage same as /weather
-❂ /wttr moon*:* Get the current status of moon
-
-*Converts*
-❂ /encrypt*:* Encrypts The Given Text
-❂ /decrypt*:* Decrypts Previously Ecrypted Text
-❂ /zip*:* reply to a telegram file to compress it in .zip format
-❂ /unzip*:* reply to a telegram file to decompress it from the .zip format
-
-*Random API*
-❂ /truth: challenge 
-❂ /dare: challenge
-Link To File:
-❂ /up: reply to a direct download link to upload it to telegram as files
- 
- File To Link:
-❂ /tgm : Get Telegraph Link Of Replied Media
-❂ /tgt: Get Telegraph Link of Replied Text
-
- *Tagger*
-❂ /all <reason> : To tag all members in the group.
-❂ /cancel : To stop tagging all members in the group.
-
-📌 /all and /cancel only can use by admin group
-"""
