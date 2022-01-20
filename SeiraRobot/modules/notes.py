151432fcfcc5c1edb0b40fdab3d93cd83cd719c9
seth <87837653+Dorimuhai@users.noreply.github.com>
2022-01-20 22:14:07 +0700

    Update notes.py

--- SeiraRobot/modules/notes.py
+++ SeiraRobot/modules/notes.py
@@ -181,7 +181,6 @@ def get(update, context, notename, show_none=True, no_format=False):
                         text,
                         reply_to_message_id=reply_id,
                         parse_mode=parseMode,
-                        disable_web_page_preview=True,
                         reply_markup=keyboard,
                     )
                 else:
@@ -191,7 +190,6 @@ def get(update, context, notename, show_none=True, no_format=False):
                         caption=text,
                         reply_to_message_id=reply_id,
                         parse_mode=parseMode,
-                        disable_web_page_preview=True,
                         reply_markup=keyboard,
                     )
 
