ddd529a38797c4a260ed2d8aeacf19eb9fdc4e89
seth <87837653+Dorimuhai@users.noreply.github.com>
2022-01-23 15:17:43 +0700

    Update __main__.py

--- SeiraRobot/__main__.py
+++ SeiraRobot/__main__.py
@@ -390,7 +390,7 @@ def seira_about_callback(update, context):
                         InlineKeyboardButton(text="Credits", callback_data="seira_credit"),
                     ],
                     [
-                        InlineKeyboardButton(text=gs(chat.id, "source_button"), url="https://github.com/Dorimuhai/SeiraXRobot"),
+                        InlineKeyboardButton(text=gs(chat.id, "Chat with Owner"), https://t.me/xyzsethhh"),
                     ],
                     [
                     InlineKeyboardButton(text=gs(chat.id, "back_button"), callback_data="seira_back"),
